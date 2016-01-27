6eb9146b7b25c162374a904dd8c5248e029b5f2c
Emanuel Ramirez <emanuelr@ucm.es>
2016-01-27 19:55:15 +0100

    Final version of Laberinto (Labyrinth) and SigueLinea(LineFollower).

--- Laberinto.c
+++ Laberinto.c
@@ -1,4 +1,7 @@
 
+// Programa para salir de laberinto con robot movil
+// Emanuel Ramirez Catapano
+
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -15,15 +18,21 @@
 #define	FALSE	            (!TRUE)
 #define CHAN_CONFIG_SINGLE  8
 #define CHAN_CONFIG_DIFF    0
-
+//para el uso de los pines que tienen señal PWM por hardware en la raspberry pi 2 se usan los GPIO 19 y 18
+#define SERVO_IZQ           19
+#define SERVO_DER           18
+#define SENSOR_LEFT          0
+#define SENSOR_FRONT          1
 static int myFd ;
 
+
+//Lectura de sensores analogicos
 char *usage = "Usage: mcp3008 all|analogChannel[1-8] [-l] [-ce1] [-d]";
 void loadSpiDriver()
 {
     if (system("gpio load spi") == -1)
     {
-        fprintf (stderr, "Can't load the SPI driver: %s\n", strerror (errno)) ;
+        //fprintf (stderr, "Can't load the SPI driver: %s\n", strerror (errno)) ;
         exit (EXIT_FAILURE) ;
     }
 }
@@ -32,10 +41,12 @@ void spiSetup (int spiChannel)
 {
     if ((myFd = wiringPiSPISetup (spiChannel, 1000000)) < 0)
     {
-        fprintf (stderr, "Can't open the SPI bus: %s\n", strerror (errno)) ;
+        //fprintf (stderr, "Can't open the SPI bus: %s\n", strerror (errno)) ;
         exit (EXIT_FAILURE) ;
     }
 }
+
+//Lectura de sensores analogicos
 
 int myAnalogRead(int spiChannel,int channelConfig,int analogChannel)
 {
@@ -47,77 +58,128 @@ int myAnalogRead(int spiChannel,int channelConfig,int analogChannel)
     return ( (buffer[1] & 3 ) << 8 ) + buffer[2]; // get last 10 bits
 }
 
-void corregirIzq(){
-    softPwmWrite(0,14);
-    softPwmWrite(1,20);
-    delay(200);
-    
-}
+
+// SERVO_IZQUIERDO > 76 hacia adelante
+// SERVO_DERECHO < 76 hacia adelante
 
 void girarDerecha(){
-    softPwmWrite(0,16)
-    softPwmWrite(1,0);
-    delay(1500); //90 grados
-    softPwmWrite(0,15);
-    softPwmWrite(1,15);
-    delay(100);
+    pwmWrite(SERVO_DER,0);
+    pwmWrite(SERVO_IZQ,0);
+    delay(300);
+    pwmWrite(SERVO_DER,77);
+    pwmWrite(SERVO_IZQ,77);
+    delay(900); //+-90 grados
 }
 
 void girarIzquierda(){
-    softPwmWrite(1,14)
-    softPwmWrite(0,0);
-    delay(1500); //90 grados
-    softPwmWrite(0,15);
-    softPwmWrite(1,15);
-    delay(100);
+    delay(650);
+    pwmWrite(SERVO_DER,69);
+    pwmWrite(SERVO_IZQ,69);
+    delay(1250); // +-90 grados
+    pwmWrite(SERVO_IZQ,77);
+    pwmWrite(SERVO_DER,69);
+    delay(1800);
 }
 
+
+//estas funciones fueron de prueba para tratar de rectificar los movimientos cambiando las velocidades de los servos
+/*
+void corregirDer(){
+    pwmWrite(SERVO_DER,70);
+    pwmWrite(SERVO_IZQ,77);
+    delay(100);
+    pwmWrite(SERVO_IZQ,77);
+    pwmWrite(SERVO_DER,69);
+    delay(200);
+}
+
+void corregirIzq(){
+    pwmWrite(SERVO_IZQ,75);
+    pwmWrite(SERVO_DER,70);
+    delay(100);
+    pwmWrite(SERVO_IZQ,77);
+    pwmWrite(SERVO_DER,69);
+    delay(200);
+}
+*/
+
+
+//Funcion principal de movimiento.
 void run(){
-    softPwmWrite(0,10);//servo izquierda
-    softPwmWrite(1,20);//servo derecha
-    int left = 0;
-    int front = 1;
-    while (myAnalogRead(spiChannel,channelConfig,left) > 370 && myAnalogRead(spiChannel,channelConfig,front)<200){ // hay que probar los valores de radio de giro y que este cerca
-        printf("Siguiendo la pared");
-    }
-    if(myAnalogRead(spiChannel,channelConfig,front) > 201){
-        printf("Giro derecha");
+    
+    
+    delay(50);
+    //Movimiento que sigue la pared.
+    pwmWrite(SERVO_IZQ,77); // >76 hacia adelante.
+    pwmWrite(SERVO_DER,69); // <76 hacia atras.
+    
+    // se mantiene mientas no se aleje o acerque demasiado, y no tenga un bloqueo en el sensor frontal.
+    while (myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) >  220 && myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) < 350
+		&& myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_FRONT)<210){ // hay que probar los valores de radio de giro y que este cerca
+        //printf("Siguiendo la pared\n");
+        
+	delay(50);
+    } // sale del bucle y busca la razon del cambio de estado.
+    
+    
+    //si detecta un objeto al frente gira a la derecha
+    if(myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_FRONT) >= 210){
+        //printf("GIRO DERECHA\n");
         girarDerecha();
+        //Llamada recursiva de la funcion.
         run();
     }
-    else if (myAnalogRead(spiChannel,channelConfig,left) < 150 && myAnalogRead(spiChannel,channelConfig,front) < 201){
-        printf("Giro Izquierda");
+    //Si detecta que la pared izquierda esta muy lejos gira a la izquierda
+    else if (myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) <= 150){
+        //printf("GIRO IZQUIERDA\n");
         girarIzquierda();
+        //Llamada recursiva de la funcion.
         run();
     }
-    else if (myAnalogRead(spiChannel,channelConfig,left) > 151 && myAnalogRead(spiChannel,channelConfig,left) < 371){
-        prinft("Corrigo izquierda");
+    
+    
+    
+    /*else if (myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) > 200  && myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) < 280){
+        //printf("Corrigo izquierda\n");
         corregirIzq();
         run();
     }
+    else if (myAnalogRead(0,CHAN_CONFIG_SINGLE,SENSOR_LEFT) >500){
+	//printf("Corrigo Derecha\n");
+	corregirDer();
+	delay(300);
+	run();
+	}*/
+    
+    
+    
+    
     else{
-        printf("STOP");
-        return;
+        //printf("Nada\n");
+	run();
     }
 }
 
 
 int main (int argc, char *argv [])
 {
-    int analogChannel=1;
-    int spiChannel=0;
-    int channelConfig=CHAN_CONFIG_SINGLE;
-    wiringPiSetup () ;
-    spiSetup(spiChannel);
-    pinMode(0,PWM_OUTPUT);
-    pinMode(1,PWM_OUTPUT);
-    softPwmCreate(1,0,100);
-    softPwmCreate(0,0,100);
+   // para el uso de los pines 18 y 19 GPIO Iniciamos wiring pi en modo GPIO
+    wiringPiSetupGpio();
+    spiSetup(0);
+    //iniciamos ambos servos en modo PWM
+    pinMode(SERVO_DER,2);
+    pwmSetMode(0);
+    pwmSetClock(400);
+    pwmSetRange(1024);
+    pinMode(SERVO_IZQ,2);
+    pwmSetMode(0);
+    pwmSetClock(400);
+    pwmSetRange(1024);
     run();
-    softPwmWrite(0,15);
-    softPwmWrite(1,15);
- 
-    delay(10000);	
+    //printf("Sale del Main");
+    pwmWrite(SERVO_DER,0);
+    pwmWrite(SERVO_IZQ,0);
+    delay(10);	
     close (myFd);
     return 0;
 }
